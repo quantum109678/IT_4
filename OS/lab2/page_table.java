74147f622fd644885f2d5b099484b14cac83d3b1
Akhil <akkashyap8991@gmail.com>
2018-01-22 13:56:58 +0530

    Page_Table

--- OS/lab2/page_table.java
+++ OS/lab2/page_table.java
@@ -4,55 +4,59 @@ public class page_table{
 
 	public static void main(String args[]){
 
-int page[MAX],i,n,f,ps,off,pno;
+int i,n,page_size,off,pno;
 
 Scanner inp=new Scanner(System.in);
 
-System.out.println("Enter the no of pages in memory");
+System.out.println("Enter the no of pages in memory:");
 
 n=inp.nextInt();
 
-System.out.println("\nEnter page size");
+int page[]=new int[n];
 
-ps=inp.nextInt();
+System.out.println("Enter page size:");
 
-System.out.println("\nEnter no of frames");
-
-f=inp.nextInt();
+page_size=inp.nextInt();
 
 for(i=0;i<n;i++)
 
 page[i]=-1;
 
-System.out.println("\nEnter the page table\n");
+System.out.println("Enter the page table");
 
-System.out.println("(Enter frame no as -1 if that page is not present in any frame)\n\n");
+System.out.println("\n(Enter frame no as -1 if that page is not present in any frame)");
 
-System.out.println("\npageno\tframeno\n-------\t-------");
+System.out.println("Page number\tFrame number");
 
 for(i=0;i<n;i++)
 {
 
-System.out.println(i);
+System.out.print("\n"+(i+1)+"\t\t");
 
 page[i]=inp.nextInt();
 
 }
-System.out.println("\n\nEnter the logical address(i.e,page no & offset):");
+System.out.println("Enter the logical address(i.e,page no & offset):");
+
+System.out.print("PageNo:");
 
 pno=inp.nextInt();
+
+pno--;
+
+System.out.print("\nOffset:");
 
 off=inp.nextInt();
 
 if(page[pno]==-1)
 
-System.out.println("\n\nThe required page is not available in any of frames");
+System.out.println("\nThe required page is not available in any of frames");
 
 else
 
-System.out.println("\n\nPhysical address(i.e,frame no & offset):"+page[pno]+off);
+System.out.println("\nCorresponding physical address: "+(page[pno]*page_size+off));
 
 
 }
-	}
+	
 }
---
+++ daa/lab2/count_inv_nsquared.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int arr[]={5,8,2,6};
+
+	static int count=0;
+
+	int n=sizeof(arr)/sizeof(arr[0]);
+
+	for(int i=0;i<n-1;i++){
+
+		for(int j=i+1;j<n;j++){
+
+			if(arr[i]>arr[j]){
+
+				++count;
+			}
+		}
+
+
+
+	}
+
+	cout<<count;
+}
