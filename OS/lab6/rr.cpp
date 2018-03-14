569c55df6de715b57a3d7ec0e10b21bfbabe6f7d
Akhil Kashyap <akkashyap8991@gmail.com>
2018-03-15 00:06:16 +0530

    Round_robin_scheduling

--- OS/lab6/rr.cpp
+++ OS/lab6/rr.cpp
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <algorithm>
+#include <queue>
 using namespace std;
-
-
 
 struct job{
 
-	int arrival,bur,com,tot,wai,sch;
+	int arrival,bur,com,tot,wai,bur_temp;
 };
 
 bool helper(job p1,job p2){
@@ -16,38 +15,19 @@ bool helper(job p1,job p2){
 	return (p1.arrival<p2.arrival);
 }
 
-int get_min(job arr[],int n){
-
-	int local_min=100,cur;
-
-	for(int i=0;i<=n;i++){
-
-		if(arr[i].sch!=1){
-
-			if(local_min>arr[i].bur){
-
-				local_min=arr[i].bur;
-
-				cur=i;
-
-			}
-		}
-
-
-	}
-
-	return (cur);
-}
+int Comp_time=0;
+queue <int> q;
 
 int main(){
 
-	int n;
-
-	static int comp_time=0;
+	int n,tq,count=0;
 
 	cout<<"Enter number of processes:\n";
 
 	cin>>n;
+
+	cout<<"Enter the Time Quantum: ";
+	cin>>tq;
 
 	job arr[n];
 
@@ -62,36 +42,37 @@ int main(){
 		cout<<"Enter burst time of process "<<i+1<<":\t";
 
 		cin>>arr[i].bur;
+		arr[i].bur_temp=arr[i].bur;
 	}
 
 	sort(arr,arr+n,helper);
 
-	int min_job;
-
-	int i=0,j=0;
-
-	while(i<n){
-
-		while(arr[j].arrival<comp_time  && j+1<n){
-
-			++j;
-		}
-
-		//cout<<j;
-
-		min_job=get_min(arr,j);
-
-		//cout<<min_job;
-
-		arr[min_job].com=comp_time+arr[min_job].bur;
-
-		comp_time=arr[min_job].com;
-
-		arr[min_job].sch=1;
-
-		i++;
+	for(int i=0;i<n;++i){
+		q.push(i);
 	}
 
+	static int comp_time=arr[0].arrival;
+	int i=q.front();
+	while(!q.empty()){
+		i=q.front();
+		q.pop();
+		cout<<i+1<<" ";
+		if(arr[i].bur<=tq){
+			comp_time+=arr[i].bur;
+			arr[i].com=comp_time;
+			arr[i].bur=0;
+		}
+		else{
+			comp_time+=tq;
+			arr[i].com+=tq;
+			arr[i].bur-=tq;
+
+		}
+		if(arr[i].bur!=0)
+			q.push(i);
+
+		++count;
+	}
 
 	for(int i=0;i<n;i++){
 
@@ -101,23 +82,18 @@ int main(){
 
 	for(int i=0;i<n;i++){
 
-		arr[i].wai=arr[i].tot-arr[i].bur;
+		arr[i].wai=arr[i].tot-arr[i].bur_temp;
 
 	}
-
-
 	
-
-	
-
 	cout<<"The scheduling is as follows:\n";
 
 	cout<<"Proc_numb Arri_time Bur_time Comp_time Tot_time Wait_time\n";
 
 	for(int i=0;i<n;i++){
 
-		cout<<"\t"<<i+1<<"\t"<<arr[i].arrival<<"\t"<<arr[i].bur<<"\t"<<arr[i].com<<"\t"<<arr[i].tot<<"\t"<<arr[i].wai<<"\n";
-
+		cout<<"\t"<<i+1<<"\t"<<arr[i].arrival<<"\t"<<arr[i].bur_temp<<"\t"<<arr[i].com<<"\t"<<arr[i].tot<<"\t"<<arr[i].wai<<"\n";
 		
 	}
+	cout<<"Context Switches: "<<count<<"\n";
 }
